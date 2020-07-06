7b2d4af3ba880435a4de6290da276ab3ee5f3f31
meddle <>
2020-07-06 18:52:12 +0000

    Added Pyspark Cell

--- SimpleDemo.py
+++ SimpleDemo.py
@@ -1,3 +1,15 @@
 # Databricks notebook source
 # MAGIC %sql
 # MAGIC select color,sum(carat) as tcarats from diamonds group by color order by tcarats
+
+# COMMAND ----------
+
+from pyspark.sql.types import DoubleType
+from pyspark.sql.functions import sum
+
+df = spark.read.table('lynch.diamonds')
+newDf = df.withColumn("caratd", df["carat"].cast("double"))
+result = newDf.groupby('color').agg(sum(newDf["caratd"]))
+
+
+display(result)
